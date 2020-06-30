b4109eacbc4c3018963296b5e08622c19a4f07a9
Nick Brown <ncbrown@cpp.edu>
2020-06-30 13:54:42 -0700

    using a static ip instead of hostname

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,6 +1,6 @@
 const urls = {
   cdn: 'https://d2b46bduclcqna.cloudfront.net',
-  api: 'https://api.legion-hq.com:3000',
+  api: 'https://44.224.76.4:3000',
   listPath: 'https://legionhq.thefifthtrooper.com/list'
 };
 
