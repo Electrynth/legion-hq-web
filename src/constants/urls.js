2508cc7820bc2ade035215608696454cb24154d4
Nick Brown <ncbrown@cpp.edu>
2020-06-30 13:36:16 -0700

    changing domains

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,7 +1,7 @@
 const urls = {
   cdn: 'https://d2b46bduclcqna.cloudfront.net',
   api: 'https://api.legion-hq.com:3000',
-  listPath: 'https://d37xhki8rk4762.cloudfront.net/list'
+  listPath: 'https://legionhq.thefifthtrooper.com/list'
 };
 
 export default urls;
