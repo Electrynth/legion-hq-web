852a0e501423e5775d4561afdcd3de48b5308a00
Nick Brown <ncbrown@cpp.edu>
2020-11-19 20:19:33 -0800

    button to toggle old points

--- src/pages/List/ListExtras/index.js
+++ src/pages/List/ListExtras/index.js
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import {
+  History as UsePrevIcon,
   Clear as ClearIcon,
   Save as SaveIcon,
   CallSplit as ForkIcon
 } from '@material-ui/icons';
+import cards from 'constants/cards';
 import DataContext from 'context/DataContext';
 import ListContext from 'context/ListContext';
 import TemplateButton from './TemplateButton';
@@ -15,6 +17,7 @@ import PrintExportButton from './PrintExportButton';
 import SimpleButton from './SimpleButton';
 
 function ListExtras() {
+  const [usingOldPoints, setUsingOldPoints] = useState(false);
   const { userId } = useContext(DataContext);
   const {
     currentList,
@@ -23,6 +26,20 @@ function ListExtras() {
     handleListSave,
     handleListFork
   } = useContext(ListContext);
+
+  const toggleUsingOldPoints = () => {
+    handleClearList();
+    Object.keys(cards).forEach(id => {
+      const card = cards[id];
+      if (card.prevCost) {
+        const temp = card.cost;
+        card.cost = card.prevCost;
+        card.prevCost = temp;
+      }
+    });
+    setUsingOldPoints(!usingOldPoints);
+  }
+
   return (
     <div
       style={{
@@ -40,13 +57,13 @@ function ListExtras() {
       <SimpleButton
         timeout={3000}
         timeoutMessage={listSaveMessage ? listSaveMessage : 'Saving...'}
-        isDisabled={!Boolean(userId)}
+        isDisabled={usingOldPoints || !Boolean(userId)}
         icon={<SaveIcon />}
         label="Save List"
         handleClick={() => handleListSave(currentList)}
       />
       <SimpleButton
-        isDisabled={!Boolean(currentList.listId)}
+        isDisabled={usingOldPoints || !Boolean(currentList.listId)}
         icon={<ForkIcon />}
         label="Fork List"
         handleClick={() => handleListFork(currentList)}
@@ -55,6 +72,13 @@ function ListExtras() {
         icon={<ClearIcon />}
         label="Clear List"
         handleClick={handleClearList}
+      />
+      <SimpleButton
+        timeout={1000}
+        timeoutMessage="Resetting list..."
+        icon={<UsePrevIcon />}
+        label={usingOldPoints ? "Using Sep 2019 Points" : "Using Nov 2020 Points"}
+        handleClick={toggleUsingOldPoints}
       />
     </div>
   );
