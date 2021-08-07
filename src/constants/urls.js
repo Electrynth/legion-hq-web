ea7fdb7a13127207bd0c4cd9fa6257de49ac9aa4
EC2 Default User <ec2-user@ip-172-31-3-67.us-east-2.compute.internal>
2021-08-07 21:49:36 +0000

    Add new CDN URL to restore images

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,6 +1,6 @@
 const urls = {
-  cdn: 'https://d2b46bduclcqna.cloudfront.net',
-  api: 'https://api.legion-hq.com:3000',
+  cdn: 'https://d2j4aq9mja0i3a.cloudfront.net',
+  api: 'https://api.legion-hq.com:3001',
   listPath: 'https://legionhq.thefifthtrooper.com/list'
 };
 
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -78,6 +78,11 @@ const routes = {
 
 const newsPosts = [
   {
+    "title": "Temporary Outage",
+    "date": "23 July 2021",
+    "body": "Accounts are back online, however card images will be unavaible for a small amount of time. Sorry for the inconvenience."
+  },
+  {
     "title": "New Cards Added",
     "date": "8 July 2021",
     "body": "New wookiee command cards for GAR added."
