86d3eaee5655279e5397ad73477c612e0a8fbfcb
Nick <nbrown4296@gmail.com>
2022-06-02 11:26:20 -0700

    Add feature to print with or without battle/command cards

--- src/pages/List/ListExtras/PrintExportButton.js
+++ src/pages/List/ListExtras/PrintExportButton.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import QRCode from 'qrcode.react';
 import { useReactToPrint } from 'react-to-print';
-import { Chip } from '@material-ui/core';
+import { Chip, Menu, MenuItem } from '@material-ui/core';
 import { Print as PrintIcon } from '@material-ui/icons';
 import { generateTournamentText } from 'constants/listOperations';
 import generateLink from './generateLink';
 
 class PrintList extends React.Component {
   render() {
-    const { currentList } = this.props;
+    const { currentList, showBattlesAndCommands = false } = this.props;
     const listLink = generateLink(currentList);
     const units = []; let printingUnits = true;
     const commands = []; let printingCommands = false;
@@ -42,21 +42,23 @@ class PrintList extends React.Component {
           {units.map((line, i) => <div key={`${line}_${i}`}>{line}</div>)}
         </div>
         <div style={{ display: 'flex', flexFlow: 'column nowrap', justifyContent: 'space-between' }}>
-          <div>
-            {commands.map((line, i) => {
-              if (line.includes('Commands:')) {
-                return <b key="commands header">Command Hand</b>;
-              }
-              return <div key={`${line}_${i}`}>{line}</div>;
-            })}
-            <div style={{ marginTop: 4 }} />
-            {battles.map((line, i) => {
-              if (line.includes('Battle Deck')) {
-                return <b key="battle deck header">Battle Deck</b>;
-              }
-              return <div key={`${line}_${i}`}>{line}</div>;
-            })}
-          </div>
+          {showBattlesAndCommands && (
+            <div>
+              {commands.map((line, i) => {
+                if (line.includes('Commands:')) {
+                  return <b key="commands header">Command Hand</b>;
+                }
+                return <div key={`${line}_${i}`}>{line}</div>;
+              })}
+              <div style={{ marginTop: 4 }} />
+              {battles.map((line, i) => {
+                if (line.includes('Battle Deck')) {
+                  return <b key="battle deck header">Battle Deck</b>;
+                }
+                return <div key={`${line}_${i}`}>{line}</div>;
+              })}
+            </div>
+          )}
           <QRCode size={147} value={listLink} />
         </div>
       </div>
@@ -66,21 +68,60 @@ class PrintList extends React.Component {
 
 function PrintExportButton({ currentList }) {
   const componentRef = React.useRef();
+  const componentRefNoBattlesCommands = React.useRef();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handlePrintMenuOpen = event => setAnchorEl(event.currentTarget);
+  const handlePrintMenuClose = () => setAnchorEl(null);
   const handlePrint = useReactToPrint({
     content: () => componentRef.current
   });
+  const handlePrintNoBattlesCommands = useReactToPrint({
+    content: () => componentRefNoBattlesCommands.current
+  });
   return (
     <React.Fragment>
+      <Menu
+        keepMounted
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handlePrintMenuClose}
+      >
+        <MenuItem
+          onClick={() => {
+            handlePrint();
+            handlePrintMenuClose();
+          }}
+        >
+          With Battle/Command Cards
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            handlePrintNoBattlesCommands();
+            handlePrintMenuClose();
+          }}
+        >
+          Without Battle/Command Cards
+        </MenuItem>
+      </Menu>
       <Chip
         clickable
         variant="outlined"
         label="Print List"
         icon={<PrintIcon />}
         style={{ marginRight: 4, marginBottom: 4 }}
-        onClick={handlePrint}
+        onClick={handlePrintMenuOpen}
       />
       <div style={{ display: 'none' }}>
-        <PrintList ref={componentRef} currentList={currentList} />
+        <PrintList
+          showBattlesAndCommands={true}
+          ref={componentRef}
+          currentList={currentList}
+        />
+        <PrintList
+          showBattlesAndCommands={false}
+          ref={componentRefNoBattlesCommands}
+          currentList={currentList}
+        />
       </div>
     </React.Fragment>
   );
